9a12fdf9ab7628a478f653d6c1188e8ca7a85d9b
Yasmin Ribeiro <yasminfrancaribeiro@gmail.com>
2024-07-05 15:08:24 -0300

    29.2 & 30.1 - WEB-JAVA-4 - Prática

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -3,10 +3,13 @@ package com.generation.blogpessoal.model;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -29,7 +32,12 @@ public class Postagem {
 
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
+	
 	public Long getId() {
 		return id;
 	}
@@ -60,6 +68,14 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
